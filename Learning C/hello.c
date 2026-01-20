1b87ab26be91e5d4de4b908cabeba8c6c3672d99
Echo-Nexus <vxattitude@gmail.com>
2026-01-20 12:26:22 +0545

    Edited Important Things and setup the environment for coding..

---
+++ Learning C/hello.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main() 
+{
+    char name[20];
+    printf(" Enter your name : \n");
+    scanf("%s", &name);
+    printf("Hello, World I am Arix Ratgaiya!\n");
+    return 0;
+}
